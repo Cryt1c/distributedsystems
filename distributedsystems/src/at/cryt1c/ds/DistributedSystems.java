7e2834e442337623b348a203cea09f3ceb4a6cde
Cryt1c <david.peherstorfer@live.de>
2014-10-03 10:47:24 +0200

    set up project

---
+++ distributedsystems/src/at/cryt1c/ds/DistributedSystems.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package at.cryt1c.ds;
+
+/**
+ * @author David
+ *
+ */
+public class DistributedSystems {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

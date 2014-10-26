222f34eeaa3030dd96bd07456f89ec45b00f569b
Cryt1c <david.peherstorfer@live.de>
2014-10-26 20:28:23 +0100

    cloud service is listening to udp and tcp port and has a list of users

--- dslab14/src/main/java/controller/CloudController.java
+++ dslab14/src/main/java/controller/CloudController.java
@@ -5,7 +5,15 @@ import util.Config;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.ServerSocket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CloudController implements ICloudControllerCli, Runnable {
 
@@ -14,6 +22,9 @@ public class CloudController implements ICloudControllerCli, Runnable {
 	private InputStream userRequestStream;
 	private PrintStream userResponseStream;
 	private ServerSocket serverSocket;
+	private DatagramSocket datagramSocket;
+	private List<User> users = new ArrayList<User>();
+	private ExecutorService executorService = Executors.newFixedThreadPool(10);
 
 	/**
 	 * @param componentName
@@ -37,16 +48,75 @@ public class CloudController implements ICloudControllerCli, Runnable {
 
 	@Override
 	public void run() {
-		// TODO
+		// adds all users from the config-file to the users-list
 		Config userconfig = new Config("user");
-		// test
-		System.out.println(userconfig.listKeys());
+		Iterator iter = userconfig.listKeys().iterator();
+		while (iter.hasNext()) {
+			String temp = iter.next().toString();
+		 	if(temp.contains("password")) {
+		 		String name;
+		 		name = temp.substring(0, temp.length() - 9);
+		 		users.add(new User(name, userconfig.getString(temp)));
+		 	}
+		}
+		// adds the credit to the user list
+		for(User user : users) {
+			user.setCredits(userconfig.getString(user.getName() + ".credits"));
+		}
+		
+		// lists the users in the list
+		try {
+			this.users();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		// creates a new TCP socket and waits for new incoming connections
 		try {
 			serverSocket = new ServerSocket(config.getInt("tcp.port"));
+			System.out.println("Socket created.");
+			
 			
 		}	catch (IOException e) {
 			throw new RuntimeException("Cannot listen on TCP port.", e);
 		}
+		executorService.execute(new Runnable() {
+			public void run() {
+				while(true) {
+					try {
+						System.out.println("serversocket accepting");
+						serverSocket.accept();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		});
+		
+		// creates a new UDP socket and waits for new incoming connections
+		try {
+			datagramSocket = new DatagramSocket(config.getInt("udp.port"));
+			System.out.println("Socket created.");
+		} catch (SocketException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		executorService.execute(new Runnable() {
+			public void run() {
+				byte[] buf = new byte[256];
+				DatagramPacket packet = new DatagramPacket(buf, buf.length);
+				while(true) {
+					try {
+						System.out.println("datagramsocket receiving");
+						datagramSocket.receive(packet);
+					} catch (IOException e) {
+						throw new RuntimeException("Cannot listen on UDP port.", e);
+					}
+				}
+			}
+		});
 		
 	}
 
@@ -59,12 +129,15 @@ public class CloudController implements ICloudControllerCli, Runnable {
 	@Override
 	public String users() throws IOException {
 		// TODO Auto-generated method stub
+		System.out.println(users);
 		return null;
 	}
 
 	@Override
 	public String exit() throws IOException {
-		// TODO Auto-generated method stub
+		serverSocket.close();
+		datagramSocket.close();
+		executorService.shutdown();
 		return null;
 	}
 
